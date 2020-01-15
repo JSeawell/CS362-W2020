a5a4f5082bbe1f9a43b419cd6e9b030ae5a858f9
JSeawell <jacob_seawell@yahoo.com>
2020-01-15 09:25:33 -0800

    updated testDom1, testDom2, and testUtil

---
+++ projects/seawellj/dominion/testDominion1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 1/15/2020
+
+@author: jseawell
+"""
+
+import Dominion
+import testUtility
+
+#initialize game setup
+player_names, supply_order, supply, players = testUtility.InitializeGame()
+
+#initialize the trash
+trash = []
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1    
+    print("\r")    
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))    
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+            
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
